a5a8192227804dc7dd588e51bcbb736925f045a2
jonathan_cornwell <jonathan.cornwell@airbnb.com>
2025-02-18 23:05:53 -0800

    add filtering

--- WatchTrackerProject/WatchTracker/WatchItem.cs
+++ WatchTrackerProject/WatchTracker/WatchItem.cs
@@ -8,23 +8,22 @@ public enum WatchItemType
     TVShow = 2
 }
 
+public class WatchItemTypeUtils {
+    public static WatchItemType? GetItemTypeFromId(string? itemTypeId)
+    {
+        return Enum.TryParse<WatchItemType>(itemTypeId, out var itemType) ? itemType : null;
+    }
+
+    public static string? GetIdFromItemType(WatchItemType? itemType)
+    {
+        return itemType?.ToString("D") ?? null;
+    }
+}
+
 public class WatchItem
 {
     public string? Title { get; set; }
     public string? Genre { get; set; }
     public string? Progress { get; set; }
     public WatchItemType? ItemType { get; set; }
-
-    // set the item type given the id in the enum (e.g. pass 2 to set it to TVShow)
-    public void SetItemTypeFromId(string? itemTypeId)
-    {
-        ItemType = Enum.TryParse<WatchItemType>(itemTypeId, out var itemType) ? itemType : null;
-    }
-
-    // get the item type as an id (e.g. returns the value 2 if the item type is TVShow)
-    public string? GetItemTypeAsId()
-    {
-        // Note: ItemType?.ToString() is NOT what we want, that returns the enum name (e.g. "TVShow") instead of the the enum value (e.g. 2)
-        return ItemType?.ToString("D") ?? null;
-    }
 }
